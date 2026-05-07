8d0e699faf1baf48ba162c44ac6dbfe6ca45cb7d
Emil Roding <emilroding27873@gmail.com>
2026-05-07 17:51:46 +1200

    connected mongo db to api

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 8080;
+const professionalRoutes = require('./routes/professional');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('/db/connect');
+j
 
 app.use(cors());
-app.use('/professional', require('./routes/professional'))
+app.use('/professional', professionalRoutes);
 
-app.listen(process.env.port || port);
-console.log('Web Server is listening at port ' + (process.env.port || port));
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(process.env.port || port);
+        console.log('Connected to MongoDB and Web Server is listening at ' + (process.env.port || port));
+    }
+});
